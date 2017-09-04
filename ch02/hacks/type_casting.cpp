9fec408f067cc669b001f2a0e7e99b56ddabaf19
Allen Hai <hai@growinfinitely.com>
2017-09-04 17:03:23 -0500

    type casting chars to ascii int equivalent

---
+++ ch02/hacks/type_casting.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  cout << "a: " << static_cast<int>('a') << endl;
+  cout << "A: " << static_cast<int>('A') << endl << endl;
+
+  cout << "b: " << static_cast<int>('b') << endl;
+  cout << "B: " << static_cast<int>('B') << endl << endl;
+
+  cout << "c: " << static_cast<int>('c') << endl;
+  cout << "C: " << static_cast<int>('C') << endl << endl;
+
+  cout << "d: " << static_cast<int>('d') << endl;
+  cout << "D: " << static_cast<int>('D') << endl << endl;
+
+  cout << "e: " << static_cast<int>('e') << endl;
+  cout << "E: " << static_cast<int>('E') << endl << endl;
+
+  cout << "f: " << static_cast<int>('f') << endl;
+  cout << "F: " << static_cast<int>('F') << endl << endl;
+
+  cout << "g: " << static_cast<int>('g') << endl;
+  cout << "G: " << static_cast<int>('G') << endl << endl;
+
+  cout << "h: " << static_cast<int>('h') << endl;
+  cout << "H: " << static_cast<int>('H') << endl << endl;
+
+  cout << "i: " << static_cast<int>('i') << endl;
+  cout << "I: " << static_cast<int>('I') << endl << endl;
+
+  cout << "j: " << static_cast<int>('j') << endl;
+  cout << "J: " << static_cast<int>('J') << endl << endl;
+
+  cout << "k: " << static_cast<int>('k') << endl;
+  cout << "K: " << static_cast<int>('K') << endl << endl;
+
+  cout << "l: " << static_cast<int>('l') << endl;
+  cout << "L: " << static_cast<int>('L') << endl << endl;
+
+  cout << "m: " << static_cast<int>('m') << endl;
+  cout << "M: " << static_cast<int>('M') << endl << endl;
+
+  cout << "n: " << static_cast<int>('n') << endl;
+  cout << "N: " << static_cast<int>('N') << endl << endl;
+
+  cout << "o: " << static_cast<int>('o') << endl;
+  cout << "O: " << static_cast<int>('O') << endl << endl;
+
+  cout << "p: " << static_cast<int>('p') << endl;
+  cout << "P: " << static_cast<int>('P') << endl << endl;
+
+  cout << "q: " << static_cast<int>('q') << endl;
+  cout << "Q: " << static_cast<int>('Q') << endl << endl;
+
+  cout << "r: " << static_cast<int>('r') << endl;
+  cout << "R: " << static_cast<int>('R') << endl << endl;
+
+  cout << "s: " << static_cast<int>('s') << endl;
+  cout << "S: " << static_cast<int>('S') << endl << endl;
+
+  cout << "t: " << static_cast<int>('t') << endl;
+  cout << "T: " << static_cast<int>('T') << endl << endl;
+
+  cout << "u: " << static_cast<int>('u') << endl;
+  cout << "U: " << static_cast<int>('U') << endl << endl;
+
+  cout << "v: " << static_cast<int>('v') << endl;
+  cout << "V: " << static_cast<int>('V') << endl << endl;
+
+  cout << "w: " << static_cast<int>('w') << endl;
+  cout << "W: " << static_cast<int>('W') << endl << endl;
+
+  cout << "x: " << static_cast<int>('x') << endl;
+  cout << "X: " << static_cast<int>('X') << endl << endl;
+
+  cout << "y: " << static_cast<int>('y') << endl;
+  cout << "Y: " << static_cast<int>('Y') << endl << endl;
+
+  cout << "z: " << static_cast<int>('z') << endl;
+  cout << "Z: " << static_cast<int>('Z') << endl << endl;
+  
+  return 0;
+}
